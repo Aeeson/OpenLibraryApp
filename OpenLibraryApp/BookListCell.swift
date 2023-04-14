cc42afcb8b96e651f7d9881d4c2bb5fbf0f2af25
Aeeson <aeeson@gmail.com>
2023-04-14 21:07:51 +0500

    Add UI for main screen

---
+++ OpenLibraryApp/BookListCell.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+class BookListCell: UITableViewCell {
+    static let identifier = "BookListCell"
+    
+    private let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .yellow
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .green
+        label.text = "Name"
+        return label
+    }()
+    
+    private let publishDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .green
+        label.text = "Publish Date"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        contentView.addSubview(coverImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(publishDateLabel)
+    }
+    
+    private func setConstraints() {
+        titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: indent).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: indent).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: coverImageView.leftAnchor, constant: -indent).isActive = true
+        
+        publishDateLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: indent).isActive = true
+        publishDateLabel.rightAnchor.constraint(equalTo: coverImageView.leftAnchor, constant: -indent).isActive = true
+        publishDateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -indent).isActive = true
+        
+        coverImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: indent).isActive = true
+        coverImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -indent).isActive = true
+        coverImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -indent).isActive = true
+    }
+    
+}
+
+let indent: CGFloat = 10
--- OpenLibraryApp/BooksListViewController.swift
+++ OpenLibraryApp/BooksListViewController.swift
@@ -2,8 +2,52 @@ import UIKit
 
 class BooksListViewController: UIViewController {
     
+    private lazy var booksList: UITableView = {
+        let tableView = UITableView()
+        tableView.register(BookListCell.self, forCellReuseIdentifier: BookListCell.identifier)
+        tableView.backgroundColor = .red
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.rowHeight = 150
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupView()
+        setConstraints()
+       
+    }
+    
+    private func setupView() {
+        view.addSubview(booksList)
+        booksList.delegate = self
+        booksList.dataSource = self
+    }
+    
+    private func setConstraints() {
+        booksList.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        booksList.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        booksList.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        booksList.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
     
 }
+
+extension BooksListViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: BookListCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailsViewController()
+        present(vc, animated: true)
+    }
+   
+}
+
